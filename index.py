a2556c0dd0ee02497de14d3a98f0a977c21318b0
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-04-20 20:44:31 -0300

    Revert "adicionando um nivel a mais em cada dificuldade"
    This reverts commit 7a4b2067726f54b191d59dc46e6e9db51c906cb6.

--- index.py
+++ index.py
@@ -34,11 +34,11 @@ def jogar():
 Digite o número correspondente ao nível que deseja: '''))
 
     if nivel == 1:
-        total_de_tentativas = 16
+        total_de_tentativas = 15
     elif nivel == 2:
-        total_de_tentativas = 11
+        total_de_tentativas = 10
     else: 
-        total_de_tentativas = 6
+        total_de_tentativas = 5
 
     '''
     Linha de código abaixo é usado no laço while
